013fe09b51dce99127932357fa2b42796ca73c72
Isaac Guerreiro <isaac@igem.org>
2022-04-14 20:31:01 -0300

    create setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'DNA Compiler uses constrain programming to facilitate genetic design.'
+LONG_DESCRIPTION = 'Gather your library, define your genetic design constrains and requirements and DNA Compiler will create the sequence for you.'
+
+setup(
+  name="dnacompiler", 
+  version=VERSION,
+  author="Isaac Guerreiro",
+  author_email="<isaacguerreirocom@gmail.com>",
+  description=DESCRIPTION,
+  long_description=LONG_DESCRIPTION,
+  packages=find_packages(),
+  install_requires=[], 
+  keywords=['python'],
+  classifiers= []
+)
